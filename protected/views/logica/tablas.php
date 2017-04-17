fce2cce808badff728b808a77148658917a93478
pegasus1992 <iguanodontecazador@gmail.com>
2017-04-16 21:30:53 -0500

    saltos de linea y demás css

--- protected/views/logica/tablas.php
+++ protected/views/logica/tablas.php
@@ -3,8 +3,6 @@
 /* @var $form CActiveForm */
 ?>
 
-<h1><?php echo Funcion::tablas; ?></h1>
-
 <div class="form-group">
 
     <?php
@@ -14,27 +12,30 @@
     ));
     ?>
 
-    <div class="col-sm-6 col-sm-offset-3">
-        <div class="tabla">
-            <input type="submit" name="lbl_p" value="p" class="btn btn-lg btn-default">
-            <input type="text" name="txt_p" value="<?php echo $p; ?>" readonly class="form-control simbolo">
-        </div>
-
-        <div class="tabla">
-            <input type="submit" name="lbl_op" value="<?php echo $op; ?>" class="btn btn-lg btn-warning">
-            <input type="hidden" name="txt_op" value="<?php echo $op; ?>">
-        </div>
-
-        <div class="tabla">
-            <input type="submit" name="lbl_q" value="q" class="btn btn-lg btn-default">
-            <input type="text" name="txt_q" value="<?php echo $q; ?>" readonly class="form-control simbolo">
-        </div>
-
-        <div class="tabla">
-            <input type="submit" name="lbl_v" value="=" class="btn btn-lg btn-success">
-            <input type="text" name="txt_v" value="<?php echo $v; ?>" readonly class="form-control simbolo">
-        </div>
-    </div>
+    <table cellSpacing="3" cellPadding="1" border="0" class="col-sm-6 col-sm-offset-3">
+        <caption align="top"><h1><?php echo Funcion::tablas; ?></h1></caption>
+        <tr>
+            <td>
+                <input type="submit" name="lbl_p" value="p" class="btn btn-lg btn-default">
+                <input type="text" name="txt_p" value="<?php echo $p; ?>" readonly class="form-control simbolo">
+            </td>
+            <td>
+                <input type="submit" name="lbl_op" value="<?php echo $op; ?>" class="btn btn-lg btn-warning">
+                <input type="hidden" name="txt_op" value="<?php echo $op; ?>">
+            </td>
+            <td>
+                <input type="submit" name="lbl_q" value="q" class="btn btn-lg btn-default">
+                <input type="text" name="txt_q" value="<?php echo $q; ?>" readonly class="form-control simbolo">
+            </td>
+            <td>
+                <input type="submit" name="lbl_v" value="=" class="btn btn-lg btn-success">
+            </td>
+            <td>
+                <label for="txt_v" class="btn btn-lg btn-default">v</label>
+                <input type="text" name="txt_v" value="<?php echo $v; ?>" readonly class="form-control simbolo">
+            </td>
+        </tr>
+    </table>
 
     <?php $this->endWidget(); ?>
 </div>

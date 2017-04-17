6b9da11e1d16d392e150a3d422ad7090946f3f9e
pegasus1992 <iguanodontecazador@gmail.com>
2017-04-16 20:57:03 -0500

    calculadora y tablas "responsive"

--- protected/views/logica/calculadora.php
+++ protected/views/logica/calculadora.php
@@ -14,61 +14,66 @@
     ));
     ?>
 
-    <div class="col-sm-8 form-inline">
-        <input type="text" name="txt_formula" id="txt_formula" value="<?php echo $formula; ?>" readonly class="form-control formula">
-        <input type="text" name="txt_valor" id="txt_valor" value="<?php echo $valor; ?>" readonly class="form-control valor">
-    </div>
-
-    <div class="col-sm-9">
-        <div class="col-sm-2">
-            <?php echo lbl_valores('p', $p); ?>
-            <?php echo lbl_valores('q', $q); ?>
-        </div>
-        <div class="col-sm-2">
-            <?php echo lbl_valores('r', $r); ?>
-            <?php echo lbl_valores('s', $s); ?>
-        </div>
-        <div class="col-sm-2">
-            <?php echo lbl_valores('t', $t); ?>
-            <?php echo lbl_valores('u', $u); ?>
-        </div>
-        <div class="col-sm-2">
-            <?php echo lbl_valores('v', $v); ?>
-            <?php echo lbl_valores('x', $x); ?>
-        </div>
-        <div class="col-sm-2">
-            <?php echo lbl_valores('y', $y); ?>
-            <?php echo lbl_valores('z', $z); ?>
-        </div>
-    </div>
-
-    <div class="col-sm-5">
-        <?php echo botonesSimbolos('x'); ?>
-        <?php echo botonesSimbolos('y'); ?>
-        <?php echo botonesSimbolos('z'); ?>
-        <?php echo botonesSimbolos('t'); ?>
-        <?php echo botonesSimbolos('u'); ?>
-        <?php echo botonesSimbolos('v'); ?>
-        <?php echo botonesSimbolos('q'); ?>
-        <?php echo botonesSimbolos('r'); ?>
-        <?php echo botonesSimbolos('s'); ?>
-        <?php echo botonesSimbolos('p'); ?>
-        <?php echo botonesSimbolos(Operaciones::$parenOp); ?>
-        <?php echo botonesSimbolos(Operaciones::$parenCl); ?>
-    </div>
-
-    <div class="col-sm-3">
-        <?php echo botonesOperaciones('del', 'DEL', 'btn btn-lg btn-warning', 'delFormula()') ?>
-        <?php echo botonesOperaciones('ac', 'AC', 'btn btn-lg btn-danger', 'acFormula()') ?>
-        <?php echo botonesOperaciones('equiv', Operaciones::$equiv, 'btn btn-lg btn-default', 'addFormula(this)') ?>
-        <?php echo botonesOperaciones('impl', Operaciones::$impl, 'btn btn-lg btn-default', 'addFormula(this)') ?>
-        <?php echo botonesOperaciones('and', Operaciones::$and, 'btn btn-lg btn-default', 'addFormula(this)') ?>
-        <?php echo botonesOperaciones('or', Operaciones::$or, 'btn btn-lg btn-default', 'addFormula(this)') ?>
-        <?php echo botonesOperaciones('not', Operaciones::$not, 'btn btn-lg btn-default', 'addFormula(this)') ?>
-        <div class="col-sm-4">
-            <input type="submit" name="btn[igual]" value="=" class="btn btn-lg btn-success">
-        </div>
-    </div>
+    <!-- http://javascripts.astalaweb.com/Calculadoras/1_Calculadoras.asp -->
+    <!-- http://javascripts.astalaweb.com/Calculadoras/Calculadora%20cient%C3%ADfica/Calculadora%20cient%C3%ADfica.htm -->
+    <!-- view-source:http://javascripts.astalaweb.com/Calculadoras/Calculadora%20cient%C3%ADfica/Calculadora%20cient%C3%ADfica.htm -->
+    <table cellSpacing="3" cellPadding="1" border="0" class="col-sm-6 col-sm-offset-3">
+        <tr>
+            <td colspan="3">
+                <input type="text" name="txt_formula" id="txt_formula" value="<?php echo $formula; ?>" readonly class="form-control">
+            </td>
+            <td colspan="2">
+                <input type="text" name="txt_valor" id="txt_valor" value="<?php echo $valor; ?>" readonly class="form-control">
+            </td>
+        </tr>
+        <tr>
+            <?php echo lbl_valores('p', $p) ?>
+            <?php echo lbl_valores('r', $r) ?>
+            <?php echo lbl_valores('t', $t) ?>
+            <?php echo lbl_valores('v', $v) ?>
+            <?php echo lbl_valores('y', $y) ?>
+        </tr>
+        <tr>
+            <?php echo lbl_valores('q', $q) ?>
+            <?php echo lbl_valores('s', $s) ?>
+            <?php echo lbl_valores('u', $u) ?>
+            <?php echo lbl_valores('x', $x) ?>
+            <?php echo lbl_valores('z', $z) ?>
+        </tr>
+        <tr>
+            <td colSpan="5"></td>
+        </tr>
+        <tr>
+            <?php echo btn_simbolos('x', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('y', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('z', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('DEL', 'btn btn-lg btn-warning', 'delFormula()'); ?>
+            <?php echo btn_simbolos('AC', 'btn btn-lg btn-danger', 'acFormula()'); ?>
+        </tr>
+        <tr>
+            <?php echo btn_simbolos('t', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('u', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('v', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos(Operaciones::$equiv, 'btn btn-lg btn-default', 'addFormula(this)') ?>
+            <?php echo btn_simbolos(Operaciones::$impl, 'btn btn-lg btn-default', 'addFormula(this)') ?>
+        </tr>
+        <tr>
+            <?php echo btn_simbolos('q', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('r', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos('s', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos(Operaciones::$and, 'btn btn-lg btn-default', 'addFormula(this)') ?>
+            <?php echo btn_simbolos(Operaciones::$or, 'btn btn-lg btn-default', 'addFormula(this)') ?>
+        </tr>
+        <tr>
+            <?php echo btn_simbolos('p', 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos(Operaciones::$parenOp, 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos(Operaciones::$parenCl, 'btn btn-lg btn-default', 'addFormula(this)'); ?>
+            <?php echo btn_simbolos(Operaciones::$not, 'btn btn-lg btn-default', 'addFormula(this)') ?>
+            <td>
+                <input type="submit" name="btn[igual]" value="=" class="btn btn-lg btn-success">
+            </td>
+        </tr>
+    </table>
 
     <?php $this->endWidget(); ?>
 </div>
@@ -76,23 +81,16 @@
 <?php
 
 function lbl_valores($sim, $val) {
-    $str = '<div class="form-inline">';
+    $str = '<td class="form-inline">';
     $str .= '<input type="submit" name="lbl[' . $sim . ']" value="' . $sim . '" class="btn btn-lg btn-default">';
     $str .= '<input type="text" name="txt[' . $sim . ']" value="' . $val . '" readonly class="form-control simbolo">';
-    $str .= '</div>';
+    $str .= '</td>';
     return $str;
 }
 
-function botonesSimbolos($sim) {
-    $str = '<div class="col-sm-4">';
-    $str .= CHtml::link($sim, '', array('class' => 'btn btn-lg btn-default', 'onclick' => 'addFormula(this)'));
-    $str .= '</div>';
-    return $str;
-}
-
-function botonesOperaciones($sim, $val, $class, $onclick) {
-    $str = '<div class="col-sm-6">';
-    $str .= CHtml::link($val, '', array('class' => $class, 'onclick' => $onclick));
-    $str .= '</div>';
+function btn_simbolos($sim, $class, $onclick) {
+    $str = '<td>';
+    $str .= CHtml::link($sim, '', array('class' => $class, 'onclick' => $onclick));
+    $str .= '</td>';
     return $str;
 }
--- protected/views/logica/tablas.php
+++ protected/views/logica/tablas.php
@@ -14,23 +14,23 @@
     ));
     ?>
 
-    <div class="col-sm-6">
-        <div class="col-sm-3">
+    <div class="col-sm-6 col-sm-offset-3">
+        <div class="tabla">
             <input type="submit" name="lbl_p" value="p" class="btn btn-lg btn-default">
             <input type="text" name="txt_p" value="<?php echo $p; ?>" readonly class="form-control simbolo">
         </div>
 
-        <div class="col-sm-3">
+        <div class="tabla">
             <input type="submit" name="lbl_op" value="<?php echo $op; ?>" class="btn btn-lg btn-warning">
             <input type="hidden" name="txt_op" value="<?php echo $op; ?>">
         </div>
 
-        <div class="col-sm-3">
+        <div class="tabla">
             <input type="submit" name="lbl_q" value="q" class="btn btn-lg btn-default">
             <input type="text" name="txt_q" value="<?php echo $q; ?>" readonly class="form-control simbolo">
         </div>
 
-        <div class="col-sm-3">
+        <div class="tabla">
             <input type="submit" name="lbl_v" value="=" class="btn btn-lg btn-success">
             <input type="text" name="txt_v" value="<?php echo $v; ?>" readonly class="form-control simbolo">
         </div>
